3ba0219177ee295a5d72aeea035a4e8947695116
Juliia Jashchuk <juli4ka.n@gmail.com>
2022-05-30 17:15:43 +0200

    nmp simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+function galleryListCreate(items) {
+    return items.map(item =>
+`<li><a class="gallery__item" href="${item.original}">
+  <img
+  class="gallery__image" src="${item.preview}"
+  data-source="${item.original}" 
+  alt="${item.description}"
+  title="${item.description}" />
+</a></li>`).join("");
+}
+galleryContainer.innerHTML = galleryListCreate(galleryItems);
+
+let gallery = new SimpleLightbox('.gallery a', { overlayOpacity: 0.8 });
+
+gallery.on('show.simplelightbox', function (e) {
+    gallery.options.fadeSpeed = 250
+    gallery.options.captionData = e.target.children[0].title
+})
+
+console.log(galleryItems);
