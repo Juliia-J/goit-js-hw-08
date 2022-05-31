537c3280a42966143c982db698793b7b0a8ff861
Juliia Jashchuk <juli4ka.n@gmail.com>
2022-05-31 15:23:16 +0200

    2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const player = new Player('handstick');
+let pauseTime = 0;
+let resumeTime = 0;
+
+const onPlay = function(data) {
+    pauseTime = data.seconds;
+    localStorage.setItem('videoplayer-current-time');
+};
+
+player.on('timeupdate', throttle(pauseTime, 1000));
+
+resumeTime = localStorage.getItem('videoplayer-current-time');
+console.log('resumeTime', resumeTime);
+
