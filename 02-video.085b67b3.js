57b8c6bf309075b4c083e70ca6900827eb1fa9b2
Juliia-J <92437435+Juliia-J@users.noreply.github.com>
2022-06-09 12:57:55 +0000

    Deploying to gh-pages from @ Juliia-J/goit-js-hw-08@4299f5f72d58e73ebfd292ded75dd6f91cfea718 🚀

---
+++ 02-video.085b67b3.js
@@ -0,0 +1,2 @@
+!function(){var e,o=document.querySelector("iframe"),r=new Vimeo.Player(o),t=new t,m=0;r.on("timeupdate",t(m,1e3)),e=localStorage.getItem("videoplayer-current-time"),console.log("resumeTime",e)}();
+//# sourceMappingURL=02-video.085b67b3.js.map
