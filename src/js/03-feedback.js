1f5ab4568edf7522250fa24d4640fee11ed10161
Juliia Jashchuk <juli4ka.n@gmail.com>
2022-06-09 13:09:49 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,22 +1,33 @@
 const refs = {
     form: document.querySelector(".feedback-form"),
-    email: document.querySelector(".feedback-form input"),
+  email: document.querySelector(".feedback-form input"),
+  textarea: document.querySelector(".feedback-form textarea"),   
 };
 
 refs.form.addEventListener("submit", onFormSubmit);
 refs.email.addEventListener("input", saveInformation);
-// refs.textarea.addEventListener("input", saveInformation);
+refs.textarea.addEventListener("input", saveInformation);
 
-function onFormSubmit(evt) {}
-evt.preventDefault();
-console.log('Відправляємо форму');
+function onFormSubmit(evt){
+  evt.preventDefault();
+  console.log('Відправляємо форму');
+  evt.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
 
 function saveInformation(evt) {
-
     const message = evt.currentTarget.value;
     console.log(message);
   localStorage.setItem('feedback-form-state', message);
-//   localStorage.setItem('LOCALSTORAGE_KEY', message.value);
-  updateOutput();
-  form.reset();
+}
+
+function popylateText() {
+  const savedText = localStorage.getItem('feedback-form-state');
+
+  if (savedText) {
+    console.log(savedText);
+    refs.email.value = savedText;
+    refs.textarea.value = savedText;
+  }
 }
