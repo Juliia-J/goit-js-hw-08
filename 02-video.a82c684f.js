f17ff31ff000bc729c3e525867251b09ca7115be
Juliia-J <92437435+Juliia-J@users.noreply.github.com>
2022-06-09 14:06:56 +0000

    Deploying to gh-pages from @ Juliia-J/goit-js-hw-08@ea6291293ad0f364469ffa74527a1e1e7eb32d8c 🚀

--- 02-video.a82c684f.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe"),o=new Vimeo.Player(e),t=new t;let r=0,l=0;o.on("timeupdate",t(r,1e3)),l=localStorage.getItem("videoplayer-current-time"),console.log("resumeTime",l);
-//# sourceMappingURL=02-video.a82c684f.js.map
