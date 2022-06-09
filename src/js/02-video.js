ea6291293ad0f364469ffa74527a1e1e7eb32d8c
Juliia Jashchuk <juli4ka.n@gmail.com>
2022-06-09 16:05:34 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,22 +1,27 @@
 
-// import Player from '@vimeo/player';
-// import throttle from 'lodash.throttle';
-// import { throttle } from 'lodash.throttle';
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
-// const player = new Player('handstick');
-const throttle = new throttle();
+const player = new Player(iframe);
+// const throttle = require('lodash.throttle');
+
 let pauseTime = 0;
 let resumeTime = 0;
 
 const onPlay = function(data) {
     pauseTime = data.seconds;
-    localStorage.setItem('videoplayer-current-time');
+    localStorage.setItem('videoplayer-current-time', pauseTime);
 };
 
 player.on('timeupdate', throttle(pauseTime, 1000));
 
 resumeTime = localStorage.getItem('videoplayer-current-time');
 console.log('resumeTime', resumeTime);
+
+player.getVideoTitle().then(function (title) {
+    console.log('title', title);
+});
+
+player.setCurrentTime(resumeTime);
 
