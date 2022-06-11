e5d490b3920dd699a11a26711474d94a81090f9b
Juliia Jashchuk <juli4ka.n@gmail.com>
2022-06-11 11:19:46 +0200

    03 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,22 +6,11 @@ const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const throttle = require('lodash.throttle');
 
-let pauseTime = 0;
-let resumeTime = 0;
-
-const onPlay = function(data) {
-    pauseTime = data.seconds;
-    localStorage.setItem('videoplayer-current-time', pauseTime);
-};
-
 player.on('timeupdate', throttle(pauseTime, 1000));
 
-resumeTime = localStorage.getItem('videoplayer-current-time');
-console.log('resumeTime', resumeTime);
+function pauseTime({ seconds }) {
+   localStorage.setItem('videoplayer-current-time', seconds); 
+}
 
-player.getVideoTitle().then(function (title) {
-    console.log('title', title);
-});
-
-player.setCurrentTime(resumeTime).then(onPlay(pauseTime));
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
 
